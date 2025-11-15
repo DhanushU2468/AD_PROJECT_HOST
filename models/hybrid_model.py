7e75483bae4991aac6668af8bdf4db5deac6f15a
Dhanush U <154718467+DhanushU2468@users.noreply.github.com>
2025-11-15 14:29:00 +0530

    Refactor HybridModel to use torchvision's resnet50

--- models/hybrid_model.py
+++ models/hybrid_model.py
@@ -1,22 +1,17 @@
 import torch
 import torch.nn as nn
-import torchvision.models as models
+from torchvision.models import resnet50
 
 class HybridModel(nn.Module):
-    def __init__(self, num_classes=4):
-        super(HybridModel, self).__init__()
-
-        # Load ImageNet pretrained ResNet50
-        self.base = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
-
-        # Convert to accept 1-channel MRI input instead of RGB
-        self.base.conv1 = nn.Conv2d(
-            1, 64, kernel_size=7, stride=2, padding=3, bias=False
-        )
-
-        # Replace final FC layer
-        self.base.fc = nn.Linear(2048, num_classes)
+    def __init__(self, num_classes=4, pretrained=True):
+        super().__init__()
+        self.cnn = resnet50(pretrained=pretrained)
+        self.cnn.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.cnn.fc = nn.Identity()
+        self.fc = nn.Linear(2048, num_classes)
 
     def forward(self, x):
-        return self.base(x)
+        x = self.cnn(x)
+        return self.fc(x)
+
 
